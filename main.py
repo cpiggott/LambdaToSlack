8a1f3965fb82cae2e346c9c6eaf1e56435bcfe2e
Chris Piggott <cpiggott@ksu.edu>
2016-05-31 02:48:33 -0500

    Updated files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 
 def handler(event, context):
-    requests.post('insert your url between these quotes',json={"text" : "Insert your message between these quotes, slack emotes such as :coffee: and slash commafds " })
+    requests.post('insert your url between these quotes',json={"text" : "Insert your message between these quotes, slack emotes such as :coffee: also work" })
